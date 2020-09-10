935768c8e44305a3786f0501ad839169c23538d6
francescomaria <francescomariafalini@akeron.com>
2020-09-11 00:55:32 +0200

    production step6

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/sory-pointer'));
+app.use(express.static(__dirname + '/dist/soryPointer'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/sory-pointer/index.html'));
+res.sendFile(path.join(__dirname+'/dist/soryPointer/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
